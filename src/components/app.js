9e4d319b03ef70bb2533e7cb809b2a278e9564ea
BIT Student <classroom@bgit.rs>
2017-11-22 09:21:56 +0100

    ready for injection

--- src/components/app.js
+++ src/components/app.js
@@ -3,20 +3,22 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import LoginPage from "./login-page/login-page";
 import MainPage from "./mainPage";
+// import AuthenticationService from '../services/authenticationService';
 
 class App extends React.Component {
     constructor(props) {
         super(props);
+
+        // this.isAuth = new AuthenticationService().isAuthenticated();
     }
 
 
     render() {
+        // this.isAuth ? <MainPage /> : <LoginPage />
         return (
+
             <div>
-                <Switch>
-                    <Route path="/" component={LoginPage} />
-                    <Route exact path="/test/test" component={MainPage} />
-                </Switch>
+                <LoginPage />
             </div>
         );
     }
