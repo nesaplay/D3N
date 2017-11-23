8d0075f13fcf75504992953299325b8dff447bbd
BIT Student <classroom@bgit.rs>
2017-11-23 16:14:22 +0100

    data service has fetchProfile, Profile DTO created

--- src/components/mainPage.js
+++ src/components/mainPage.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
 import Header from "./common/header";
 
-
 class MainPage extends Component {
     constructor(props) {
         super(props);
+
     }
 
     render() {
+        
         return (
             <main>
                 <Header />
