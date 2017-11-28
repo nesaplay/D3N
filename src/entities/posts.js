3fdcaa13470039707c8077b6dbae9dcc565ca8ea
BIT Student <classroom@bgit.rs>
2017-11-28 11:52:23 +0100

    feed connected to textPost DTO

--- src/entities/posts.js
+++ src/entities/posts.js
@@ -2,6 +2,8 @@ class Post  {
     constructor(post) {
         this.id = post.id;
         this.text = post.text;
+        this.type = post.type;
+        this.commentsNum = post.commentsNum;
         
     }
 }
